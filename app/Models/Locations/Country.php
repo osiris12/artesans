5ccc4ebf31b939e6b58221f522398af58e1d57d0
Osiris Robledo <robledoosiris@gmail.com>
2023-04-13 22:39:12 +0000

    Create location models and migrations

--- app/Models/Locations/Country.php
+++ app/Models/Locations/Country.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Locations;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Location extends Model
+class Country extends Model
 {
     use HasFactory;
 }
