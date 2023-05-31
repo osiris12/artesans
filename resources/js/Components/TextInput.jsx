7313ee0657a11e17081803d57f6ea9f97af62887
Osiris Robledo <robledoosiris@gmail.com>
2023-05-31 00:16:10 +0000

    Create new register form based of off original. Edit PrimaryButton and TextInput components with new tailwind styling.

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,23 +1,20 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import {forwardRef, useEffect, useRef} from 'react';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
-    const input = ref ? ref : useRef();
+export default forwardRef(function TextInput({type = 'text', className = '', isFocused = false, ...props}, ref) {
+  const input = ref ? ref : useRef();
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
-        }
-    }, []);
+  useEffect(() => {
+    if (isFocused) {
+      input.current.focus();
+    }
+  }, []);
 
-    return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                className
-            }
-            ref={input}
-        />
-    );
+  return (
+    <input
+      {...props}
+      type={type}
+      className="w-full mt-5 px-8 py-1 md:py4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+      ref={input}
+    />
+  );
 });
