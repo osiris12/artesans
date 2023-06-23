a0c595245b34f78f3afcfe02df5b207732eea150
Osiris Robledo <robledoosiris@gmail.com>
2023-06-23 04:13:48 +0000

    Add Google Maps Marker Clusters to improve map performance.

--- resources/js/Components/Map.jsx
+++ resources/js/Components/Map.jsx
@@ -1,11 +1,11 @@
-import {GoogleMap, InfoWindow, Marker} from "@react-google-maps/api";
+import {GoogleMap, InfoWindow, Marker, MarkerClusterer} from "@react-google-maps/api";
 import {useMemo, useState} from "react";
 import {Link} from "@inertiajs/react";
 
 export default function Map({locations}) {
   const center = useMemo(() => ({
-    lat: 21.883148,
-    lng: -102.296260
+    lat: 6.083148,
+    lng: -101.296260,
   }), []);
 
   const options = useMemo(() => ({
@@ -23,35 +23,41 @@ export default function Map({locations}) {
     setSelectedLocation(null);
   };
 
+  function createKey(location) {
+    return location.lat + location.lng
+  }
+
 
   return (
     <>
       <div className="relative h-750">
         <GoogleMap
-          zoom={5}
+          zoom={4.2}
           center={center}
           options={options}
           mapContainerClassName="absolute h-full w-full bottom-0 lg:right-0 lg:h-full lg:w-3/6"
         >
-          {locations && locations.map((location, index) => {
-            let lat = location.latitude;
-            let lng = location.longitude;
-            return (
-              <Marker
-                position={{lat: lat, lng: lng}}
-                key={index} clickable={true}
-                onClick={() => handleMarkerClick(location)}
-              >
-                {selectedLocation && selectedLocation === location && (
-                  <InfoWindow onCloseClick={handleInfoWindowClose}>
-                    <div>
-                      <Link href={"/artesans/1"}>Address: {location.address}</Link>
-                    </div>
-                  </InfoWindow>
-                )}
-              </Marker>
-            );
-          })}
+          <MarkerClusterer>
+            {(clusterer) =>
+              locations.map((location, index) => (
+                <Marker
+                  position={{lat: location.latitude, lng: location.longitude}}
+                  key={index}
+                  clickable={true}
+                  onClick={() => handleMarkerClick(location)}
+                  clusterer={clusterer}
+                >
+                  {selectedLocation && selectedLocation === location && (
+                    <InfoWindow onCloseClick={handleInfoWindowClose}>
+                      <div>
+                        <Link href={"/artesans/1"}>Address: {location.address}</Link>
+                      </div>
+                    </InfoWindow>
+                  )}
+                </Marker>
+              ))
+            }
+          </MarkerClusterer>
         </GoogleMap>
       </div>
     </>
